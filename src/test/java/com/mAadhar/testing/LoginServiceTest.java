0d7bac4561bdeaabe31d0d3cfea3b2f33b0eb96d
Raheem-Sk <raheemshaik276@gmail.com>
2023-09-15 19:07:18 +0530

    capstone project

---
+++ src/test/java/com/mAadhar/testing/LoginServiceTest.java
@@ -0,0 +1,21 @@
+package com.mAadhar.testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class LoginServiceTest {
+	WebDriver driver;
+
+	
+	@Test
+	public void signInTest() throws InterruptedException {
+		
+	}
+
+	@Test
+	public void signUpTest() {
+		
+	}
+}
---
+++ src/test/java/com/mAadhar/testing/RequestServiceTest.java
@@ -0,0 +1,36 @@
+package com.mAadhar.testing;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+
+public class RequestServiceTest {
+	WebDriver driver;
+
+
+  @Test
+  public void AllAadhars() {
+	  
+  }
+
+ 
+
+  @Test
+  public void getAllRequestsTest() {
+	  
+  }
+
+
+  @Test
+  public void ApplyAadharTest() {
+	  
+  }
+
+  @Test
+  public void viewmyAdharTest() {
+	  
+  }
+}
